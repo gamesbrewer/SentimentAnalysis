c1444afc3b1176a65f58a8de1365e53cc37b23ec
Sudarshan <sudarshan@spark.com.np>
2017-03-30 14:42:57 +0545

    training model added

--- Sentiment_analysis_custom_data/create_sentiment_featuresets.py
+++ Sentiment_analysis_custom_data/create_sentiment_featuresets.py
@@ -92,7 +92,7 @@ def create_feature_sets_and_labels(pos, neg, test_size=0.1):
     return train_x, train_y, test_x, test_y
 
 
-if __name__ == '__main__':
-    train_x, train_y, test_x, test_y = create_feature_sets_and_labels('pos.txt', 'neg.txt')
-    with open('sentiment_set.pickle', 'wb') as f:
-        pickle.dump([train_x, train_y, test_x, test_y], f)
+# if __name__ == '__main__':
+#     train_x, train_y, test_x, test_y = create_feature_sets_and_labels('pos.txt', 'neg.txt')
+#     with open('sentiment_set.pickle', 'wb') as f:
+#         pickle.dump([train_x, train_y, test_x, test_y], f)
